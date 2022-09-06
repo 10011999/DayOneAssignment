6df8ced194ed43bad22b7a28ea58920793130534
10011999 <motewaraditya77@gmail.com>
2022-09-06 14:56:03 +0530

    Added:Area and perimeter of triangle

--- src/com/bridgelabz/DayOneAssignment.java
+++ src/com/bridgelabz/DayOneAssignment.java
@@ -6,6 +6,7 @@ public class DayOneAssignment {
     public static void main(String[] args) {
         DayOneAssignment dayOneAssignment=new DayOneAssignment();
         dayOneAssignment.rectangle();
+        dayOneAssignment.triangle();
     }
     public void rectangle(){
         Scanner sc = new Scanner(System.in);
@@ -16,5 +17,18 @@ public class DayOneAssignment {
         System.out.println(areaOfRectangle);
         int perimeterOfRectangle = 2 * length + 2 * breadth;
         System.out.println(perimeterOfRectangle);
+    }
+    public void triangle(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a hight and base");
+        double hight = sc.nextDouble();
+        double base =sc.nextDouble();
+        double areaOfTriangle = (base * hight) / 2;
+        System.out.println("Enter side,side1,base1");
+        double side = sc.nextDouble();
+        double side1 = sc.nextDouble();
+        double base1 = sc.nextDouble();
+        double perimeterOfTrangle = side + side1 + base1;
+        System.out.println("Area of triangle " +areaOfTriangle+ " Perimeter of trangle " +perimeterOfTrangle);
     }
 }
